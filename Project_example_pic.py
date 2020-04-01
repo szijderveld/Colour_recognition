293ed92bf8ad0b02e0b13891c51f260020472dff
Sam Zijderveld <59475770+SaveTheOceans1@users.noreply.github.com>
2020-04-01 23:37:44 +0100

    d
    d

---
+++ Project_example_pic.py
@@ -0,0 +1,98 @@
+import cv2
+import pandas as pd
+
+
+
+# import argparse
+# ap = argparse.ArgumentParser()
+# ap.add_argument('-i', '--image', required=True, help="Image Path")
+# args = vars(ap.parse_args())
+# img_path = args['image']
+
+# #Reading image with opencv
+# img = cv2.imread(img_path)
+
+#Read image with opencv
+img = cv2.imread('./InputData/colorpic2.jpg')
+
+#Reading csv file with pandas and giving names to each column
+index=["color","color_name","hex","R","G","B"]
+csv = pd.read_csv('./InputData/colors.csv', names=index, header=None)
+
+
+
+#declare global variables
+clicked = False
+r = g = b = xpos = ypos = 0
+
+
+
+
+
+def draw_function(event, x, y, flags, param):
+    '''
+    A fuction to pull information from the image, this information includes x and y coordinant of the double click. Upon the event of a double click the r g b values are updated to the 
+    clicked locations b g r values.
+
+    Input variables defined by cv2.setMousCallback
+    '''
+    if event == cv2.EVENT_LBUTTONDBLCLK:
+        global b,g,r,xpos,ypos, clicked
+        clicked = True
+        xpos = x
+        ypos = y
+        b,g,r = img[y,x]
+        b = int(b)
+        g = int(g)
+        r = int(r)
+
+
+def getColorName(R, G, B):
+    '''
+    The fuction searchs the color database seaching for the closest net match to of the input R G B values. 
+    This scaling is achieved using linear scaling.
+    '''
+    minimum = 10000
+    for i in range(len(csv)):
+        d = abs(R - int(csv.loc[i,"R"])) + abs(G - int(csv.loc[i,"G"]))+ abs(B - int(csv.loc[i,"B"]))
+        if(d<=minimum):
+            minimum = d
+            cname = csv.loc[i,"color_name"]
+    return cname
+
+
+#create window and set mouse actions
+cv2.namedWindow('image')
+cv2.setMouseCallback('image',draw_function)
+
+
+
+
+#Setup infinite look
+while(1):
+
+    #open window
+    cv2.imshow("image",img)
+    
+    #if clicked inpute = True, setup a information windows to display the color
+    if (clicked):
+
+        #cv2.rectangle(image, startpoint, endpoint, color, thickness) -1 thickness fills rectangle entirely
+        cv2.rectangle(img,(20,20), (750,60), (b,g,r), -1)
+        #Creating text string to display ( Color name and RGB values )
+        text = getColorName(r,g,b) + ' R='+ str(r) + ' G='+ str(g) + ' B='+ str(b)
+        #cv2.putText(img,text,start,font(0-7), fontScale, color, thickness, lineType, (optional bottomLeft bool) )
+        cv2.putText(img, text,(50,50),2,0.8,(255,255,255),2,cv2.LINE_AA)
+
+        #For very light colours we will display text in black colour
+        if(r+g+b>=600):
+            cv2.putText(img, text,(50,50),2,0.8,(0,0,0),2,cv2.LINE_AA)
+        clicked=False
+
+
+    #Break loop and closes window when esc is pressed.
+    if cv2.waitKey(20) & 0xFF ==27:
+        break
+
+
+cv2.destroyAllWindows()
